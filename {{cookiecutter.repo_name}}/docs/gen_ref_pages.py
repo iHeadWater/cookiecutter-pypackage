9d5eab2590c3dec104cb7f00e40625136f4ccfe1
forestbat <k678900000@163.com>
2022-07-12 21:24:12 +0800

    Update gen_ref_pages.py

--- {{cookiecutter.repo_name}}/docs/gen_ref_pages.py
+++ {{cookiecutter.repo_name}}/docs/gen_ref_pages.py
@@ -4,9 +4,9 @@ from pathlib import Path
 import mkdocs_gen_files
 
 nav = mkdocs_gen_files.Nav()
-for path in sorted(Path("src").rglob("*.py")):
-    module_path = path.relative_to("src").with_suffix("")
-    doc_path = path.relative_to("src").with_suffix(".md")
+for path in sorted(Path("{{cookiecutter.repo_name}}").rglob("*.py")):
+    module_path = path.relative_to("{{cookiecutter.repo_name}}").with_suffix("")
+    doc_path = path.relative_to("{{cookiecutter.repo_name}}").with_suffix(".md")
     full_doc_path = Path("reference", doc_path)
     parts = tuple(module_path.parts)
     if parts[-1] == "__init__":
